68756bc529bc3c062585f4250dc907cc6155423b
JT Kang <kjt1112@gmail.com>
2016-10-25 23:44:37 +0900

    Fix code

--- git-tweak.sh
+++ git-tweak.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-curl -o ~/.gitignore_template https://raw.githubusercontent.com/myurd/git-tweak/master/ignore/ALL.gitignore
-git config --global alias.new '!git init && cp ~/.gitignore_template ./ && git add .gitignore && git commit -m "Init gitignore"'
-
+curl https://raw.githubusercontent.com/myurd/git-tweak/master/ignore/ALL.gitignore -o ~/.gitignore_template
+git config --global alias.new '!git init && cp ~/.gitignore_template ./.gitignore && git add .gitignore && git commit -m "Init gitignore"'
 #EOF
